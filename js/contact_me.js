fbedbd2193310d1a234f96e6ae0ccbebfb9fbb8a
LokeshRs <lokesh.rs12@gmail.com>
2019-01-21 15:24:58 +0530

    contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("enable", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://test.formspree.io/",
         type: "POST",
         data: {
           name: name,
